e61f391a3c0bd96e4483011dd950c6e98e6fcfa2
oamo01 <omar.mariscal@nimanic.com>
2017-03-13 18:14:28 -0600

    Update at.sh

--- at.sh
+++ at.sh
@@ -1,12 +1,11 @@
 #! /bin/sh
 
-rm  /boot/at.txt
+rm  /boot/info.txt
 
-cat /dev/ttyUSB2 >> /boot/at.txt &
+cat /dev/ttyUSB2 >> /boot/info.txt &
 sleep 1
-chat -V -s '' "AT+CSQ" '' > /dev/ttyUSB2 < /dev/ttyUSB2
+chat -V -s '' "$1" '' > /dev/ttyUSB2 < /dev/ttyUSB2
 sleep 2
 killall cat
-more /boot/at.txt
+more /boot/info.txt
 exit 0
-
